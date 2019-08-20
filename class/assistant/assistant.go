b72c086d4fdb762bf2924ef1468bd07f06c39985
psh <psh@p2p.p2p>
2019-08-20 15:25:27 +0300

    update

--- class/assistant/assistant.go
+++ class/assistant/assistant.go
@@ -4,10 +4,19 @@ import (
 	"../../webserver/handlers"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func ParseJson(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+		//panic(err)
+	}
+	log.Println(string(body))
+
 	decoder := json.NewDecoder(r.Body)
 	fmt.Printf("//// %+v //// %+v \n", r.Body, decoder)
 	var t request
