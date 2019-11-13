24af3acb455ac9b682c546859830bf8119588817
psh <psh@p2p.p2p>
2019-11-13 20:20:55 +0300

    update

--- db/postgres/device.go
+++ db/postgres/device.go
@@ -39,7 +39,7 @@ func GetCapabilities(deviceId string) ([]types.DBCapabilities, error) {
 
 func SetState(id, capabilities, state string) error {
 	fmt.Println("Set State device:", id, capabilities)
-	_, err := DBX.Exec("UPDATE capabilities SET state = $1 WHERE id = $2 and type = $3;", state, id, capabilities)
+	_, err := DBX.Exec("UPDATE capabilities SET state = $1 WHERE device_id = $2 and type = $3;", state, id, capabilities)
 	if err != nil {
 		fmt.Println("postgres UpdateState: ", err)
 		return err
